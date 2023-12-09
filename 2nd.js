c85dda192620936d72345fa0528b1ddd44ef7522
alishbasheikhusman <152896627+alishbasheikhusman@users.noreply.github.com>
2023-12-09 06:47:24 -0800

    Add files via upload
    done assignment

---
+++ 2nd.js
@@ -0,0 +1,101 @@
+
+// 1st chap example
+
+// alert('boring assignment and also javascprit');
+
+// 2nd chap example
+// var a = 'alishbaUsman';
+// console.log(a);
+// var b = 'my age is 22 years';
+// console.log(b);
+
+// 3rd chap example
+// var weight = 20;
+// var length = 31;
+// alert(weight + length);
+
+
+// chap  4 just querry
+
+
+// 5 chap example
+// var popularNumber = 4;
+// var popularNumber = 2 + 2;
+// console.log(popularNumber);
+
+
+// var sup = 20;
+// var sup = 20 - 5;
+// alert(sup);
+
+
+// chap 15 array
+
+// let marks = [30, 50, 90, 60, 40, 78, 'Nan'];
+
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+// console.log(marks[4]);
+// console.log(marks[6] = 100);
+// marks[0] = 70;
+
+// console.log(marks);
+
+
+// ary method
+// let num = [1, 2, 3, 4, 5, 56, 88,];
+// let b = num.toString();
+// console.log(b);
+// let c = num.join('and');
+// console.log(c);
+
+
+
+// let a = 30;
+// let b = 20;
+
+// console.log(a == b);
+
+// let c = 10;
+// let d = '10';
+// console.log(c  === d);
+
+// let e = 20;
+// let f = 10;
+// console.log(e > f);
+
+// var a = ['abs', 'sbg', 'jdh', 'hdg'];
+
+
+//  var i = a.indexOf('jdh');
+// console.log(i);
+
+
+// how to reverse
+var a = ['abs', 'sbg', 'jdh', 'hdg'];
+var rev = a.reverse();
+console.log(rev);
+
+// how to join aary (just use string)
+var a = ['abs', 'sbg', 'jdh', 'hdg'];
+var rev = a.join("-");
+console.log(rev);
+
+// how to combine arry string and number
+var a = ['abs', 'sbg', 'jdh', 'hdg'];
+var b = [1, 2, 56, 78,];
+var combineArry = a.concat(b);
+console.log(combineArry);
+
+// how to search any string  in arry
+// var existing = a.includes('qwe');
+// console.log(existing);
+
+// for (var i = 0; i < 10; i++) {
+//     for (var j = 0; j < 5; j++) {
+//         console.log(j, i);
+//     }
+// }
+
